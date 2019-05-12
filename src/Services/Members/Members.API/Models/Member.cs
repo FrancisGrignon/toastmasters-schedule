5e4aa3310b286842f4e32652e7b99749fbda0888
Francis Grignon <live.com@ncis.ca>
2019-05-12 10:20:43 -0400

    Add members API

---
+++ src/Services/Members/Members.API/Models/Member.cs
@@ -0,0 +1,13 @@
+namespace Grignon.Toastmasters.Services.Members.API.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Note { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
---
+++ src/Services/Members/Members.DataAccess/MemberContext.cs
@@ -0,0 +1,25 @@
+﻿using Members.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Members.DataAccess.Persistence
+{
+    public class MemberContext : DbContext
+    {
+        public MemberContext(DbContextOptions<MemberContext> options) : base(options)
+        {
+            // Empty
+        }
+
+        public DbSet<Member> Members { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            {
+                var connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Members;Integrated Security=True";
+
+                optionsBuilder.UseSqlServer(connection);
+            }
+        }
+    }   
+}
