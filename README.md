974831fe2e366cb05155f24af3419c390a87c682
Francis Grignon <francis.grignon@canada.ca>
2019-05-13 19:03:15 -0400

    svsd

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
 # toastmasters-schedule
 Website used to manage meeting and send reminders.
+
+## Notes
+
+
+View calendar
+Manage members / use toastmasters api
+Manage meetings
+
+Take a role
+Free a role
+Decline a role
+Create Meeting
+
+
+Send Reminder
+Parse email
+
+Roles statistics
+
+
+
+Role
+{
+	RoleId
+	Name
+	Description
+
+	CreatedAt
+	CreatedBy
+	UpdatedAt
+	UpdatedBy
+	Alive
+}
+
+Branch
+{
+    PathwaysId
+    Name
+    Note
+
+    CreatedAt
+	CreatedBy
+	UpdatedAt
+	UpdatedBy
+	Alive
+}
+
+Speech
+{
+    SpeechId
+    MeetingId
+    MeetingSpeakerId
+    Name
+    Note
+    PathwaysId
+    PathwaysLevel
+
+	CreatedAt
+	CreatedBy
+	UpdatedAt
+	UpdatedBy
+	Alive
+}
+
+
+Meeting
+{
+	MeetingId
+	Date
+	Name
+    Note
+
+	Toastmaster
+	Humorist
+	Linguist
+
+	Topicsmaster
+	  Table Topics Speaker
+
+	Meeting Speaker 1
+
+	Meeting Speaker 2
+
+	General Evaluator
+      Evaluator 1
+	  Evaluator 2
+	  Grammarian
+	  Ah-Counter
+	  Timer
+
+	CreatedAt
+	CreatedBy
+	UpdatedAt
+	UpdatedBy
+	Alive
+}
+
+Member
+{
+	MemberId
+	Name
+    Note
+
+	Email
+
+	CreatedAt
+	CreatedBy
+	UpdatedAt
+	UpdatedBy
+	Alive
+}
--- src/Services/Meetings/Meetings.API/Startup.cs
+++ src/Services/Meetings/Meetings.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Meetings.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -32,6 +33,10 @@ namespace Meetings.API
             var connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Meetings;Integrated Security=True";
 
             services.AddDbContext<MeetingContext>(options => options.UseSqlServer(connection));
+
+            services
+                .AddCustomOptions(Configuration)
+                .AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,8 +52,67 @@ namespace Meetings.API
                 app.UseHsts();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Toastmasters - Meetings HTTP API");
+                c.RoutePrefix = string.Empty;
+            });
+
+
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+    }
+
+
+    public static class CustomExtensionMethods
+    {
+        public static IServiceCollection AddCustomOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MeetingSettings>(configuration);
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Instance = context.HttpContext.Request.Path,
+                        Status = StatusCodes.Status400BadRequest,
+                        Detail = "Please refer to the errors property for additional details."
+                    };
+
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json", "application/problem+xml" }
+                    };
+                };
+            });
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "Toastmasters - Members HTTP API",
+                    Version = "v1",
+                    Description = "The Members Microservice HTTP API.",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
+
+            return services;
+
         }
     }
 }
