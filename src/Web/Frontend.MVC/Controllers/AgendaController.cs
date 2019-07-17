480dfe61637c15ebe536eee484c099e6b2dc6c82
Francis Grignon <live.com@ncis.ca>
2019-07-16 21:50:26 -0400

    Microsoft.AspNetCore.Cors

---
+++ src/Web/Frontend.MVC/Controllers/AgendaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Frontend.MVC.Controllers
+{
+    public class AgendaController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public AgendaController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET: Agenda/{id}
+        public async Task<ActionResult> Index(int id)
+        {
+            var client = new MeetingClient(_config);
+
+            var meeting = await client.Get(id);
+
+            if (null == meeting)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+    }
+}
---
+++ src/Web/Frontend.MVC/Models/AgendaViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.MVC.Models
+{
+    public class AgendaViewModel
+    {
+        public DateTime Date { get; set; }
+
+
+    }
+
+    public class Block
+    {
+        public DateTime Time { get; set; }
+        public string Title { get; set; }
+        public string Member { get; set; }
+        public int Duration { get; set; }
+
+        ICollection<Labor> Labors { get; set; }
+    }
+
+    public class Labor
+    {
+        public string Member { get; set; }
+
+        public int Duration { get; set; }
+
+        public int Role { get; set; }
+        
+        public int Order { get; set; }
+    }
+}
